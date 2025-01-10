19a0a03e08624f407abd5d61bb61189c0d0f4c6c
kutayozcepne <kutayozcepne@gmail.com>
2025-01-10 16:27:23 +0300

    model yükleme
    pulley modelini yükleyebiliyoruz ve scale edebiliyoruz

--- pulley-puzzle/src/levels/level.js
+++ pulley-puzzle/src/levels/level.js
@@ -36,10 +36,10 @@ export class Level{
         let position;
         switch (obj.type) {
             case "pulley":
-                position = obj.position
-                position[2] += this.pos
-                object = new Pulley(this.scene ,position);
-                this.objects.push(object)
+                position = obj.position;
+                position[2] += this.pos; // Adjust based on the level offset
+                object = new Pulley(this.scene, position, 10);
+                this.objects.push(object);
                 break;
             case "weight":
                 position = obj.position
--- pulley-puzzle/src/objects/Pulley.js
+++ pulley-puzzle/src/objects/Pulley.js
@@ -2,37 +2,38 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 export class Pulley {
-    constructor(scene, position = [ 5,  5,  0 ], material) {
-
+    constructor(scene, position = [5, 5, 0], scale = 1) {
         this.scene = scene;
-        this.model;
+        this.model = null;
+
         const loader = new GLTFLoader();
-        loader.load( '../public/models/pulley.glb', function ( glb ) {
-
+        loader.load('../public/models/pulley.glb', (glb) => {
             this.model = glb.scene;
-            this.model.position.set(10,10,0);
+
+            // Set the initial position
+            this.model.position.set(position[0], position[1], position[2]);
+
+            // Set the initial scale
+            this.model.scale.set(scale, scale, scale);
+
+            // Add to the scene
             scene.add(this.model);
+        }, undefined, (error) => {
+            console.error('An error occurred while loading the pulley model:', error);
+        });
+    }
 
-            } );
-
-
-
-            if (material) {
-            this.material = material;
-        }else{
-            this.material = new THREE.MeshBasicMaterial({ color: 0xffd700 });
+    setScale(scale) {
+        if (this.model) {
+            this.model.scale.set(scale, scale, scale);
+        } else {
+            console.warn('Pulley model is not loaded yet. Scale cannot be set.');
         }
-        // Create the pulley geometry
-        const geometry = new THREE.CylinderGeometry(2, 2, 0.5, 32);
-
-        this.mesh = new THREE.Mesh(geometry, this.material);
-
-        this.mesh.position.set(position[0], position[1], position[2]);
-        this.mesh.rotation.x = Math.PI / 2;
-        scene.add(this.mesh);
     }
 
     rotate(amount) {
-        this.mesh.rotation.z += amount;
+        if (this.model) {
+            this.model.rotation.z += amount;
+        }
     }
 }
